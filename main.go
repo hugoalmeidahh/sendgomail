cc51b68a43ae71a7cc1d77dd90a3dc9caabd409c
Hugo Almeida <hugoalmeidahh@gmail>
2022-02-07 01:45:53 -0300

    add send mail with secure ssl

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 		"hugoalmeidahh@gmail.com",
 	}
 
+	sendMailWithSSL(emails)
+
 	fmt.Print(len(emails))
 	fmt.Print(emails[0])
 	for i := 0; i < len(emails); i++ {
@@ -19,11 +21,12 @@ func main() {
 		}
 
 		fmt.Print((emails[i]))
-		sendEmail(e)
+		sendMailWithoutSSL(e)
+
 	}
 }
 
-func sendEmail(to []string) {
+func sendMailWithoutSSL(to []string) {
 	from := "contato@hugoalmeidahh.com"
 
 	user := "contato@hugoalmeidahh.com"
@@ -46,4 +49,89 @@ func sendEmail(to []string) {
 	}
 
 	fmt.Println("Email sent successfully")
+}
+
+
+func sendMailWithSSL(toMail []string) {
+	fmt.Println("Send mail")
+
+	srtMail := strings.Join(toMail, ", ")
+
+	fmt.Println(srtMail)
+
+	from := mail.Address{"", "contato@host.com"}
+	to := mail.Address{"", srtMail}
+	subj := "This is the email subject test"
+	body := "This is an example body.\n With two lines."
+
+	// Setup headers
+	headers := make(map[string]string)
+	headers["From"] = from.String()
+	headers["To"] = to.String()
+	headers["Subject"] = subj
+
+	// Setup message
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	// Connect to the SMTP Server
+	servername := "mail.host.com:465"
+
+	host, _, _ := net.SplitHostPort(servername)
+
+	auth := smtp.PlainAuth("", "contato@host.com", "xpto@qwerty", host)
+
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         host,
+	}
+
+	// Here is the key, you need to call tls.Dial instead of smtp.Dial
+	// for smtp servers running on 465 that require an ssl connection
+	// from the very beginning (no starttls)
+	conn, err := tls.Dial("tcp", servername, tlsconfig)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	c, err := smtp.NewClient(conn, host)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Auth
+	if err = c.Auth(auth); err != nil {
+		log.Panic(err)
+	}
+
+	// To && From
+	if err = c.Mail(from.Address); err != nil {
+		log.Panic(err)
+	}
+
+	if err = c.Rcpt(to.Address); err != nil {
+		log.Panic(err)
+	}
+
+	// Data
+	w, err := c.Data()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	c.Quit()
 }
